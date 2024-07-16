afbbb6755755a75a45d3d2cad6e622be6d4f93fa
Prajwal-PL <prajwalpl096@gmail.com>
2024-07-16 21:23:47 +0530

    Added role based access to navbar component

---
+++ src/components/global/Navbar2.tsx
@@ -0,0 +1,116 @@
+"use client";
+import { navigation } from "@/lib/utils";
+import clsx from "clsx";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import Image from "next/image";
+import { CreditCard, LogOut, Settings, User } from "lucide-react";
+import { SignOut } from "@/app/actions/auth.actions";
+
+type Props = {
+  role: string;
+};
+
+const Navbar2 = ({ role }: Props) => {
+  const pathName = usePathname();
+  const session = useSession();
+
+  return (
+    <div className="p-4 flex items-center justify-between sticky top-0 backdrop-blur-md backdrop-brightness-75 border-b">
+      <aside className="flex items-center gap-2">
+        <Link href="/">
+          <span className="text-xl text-primary font-bold">Flow.</span>
+        </Link>
+      </aside>
+
+      <nav className="md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
+        <ul className="flex items-center justify-center md:gap-6 gap-3">
+          {navigation.map((link) => (
+            <Link
+              className={clsx(
+                "hover:border-b hover:text-primary hover:py-5 hover:border-primary",
+                {
+                  "text-primary py-5 border-b border-primary hover:border-orange-400 hover:text-orange-400":
+                    pathName === link.href,
+                }
+              )}
+              key={link.title}
+              href={link.href}
+            >
+              {link.title}
+            </Link>
+          ))}
+          {role === "ADMIN" ||
+            (role === "SELLER" && (
+              <Link
+                href="/add"
+                className={clsx(
+                  "hover:border-b hover:text-primary hover:py-5 hover:border-primary",
+                  {
+                    "text-primary py-5 border-b border-primary hover:border-orange-400 hover:text-orange-400":
+                      pathName === "/add",
+                  }
+                )}
+              >
+                Add
+              </Link>
+            ))}
+        </ul>
+      </nav>
+      <aside className="flex gap-2 items-center">
+        {session.data?.user && (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Image
+                src={session.data?.user?.image || ""}
+                alt="profile pic"
+                className="rounded-full cursor-pointer shrink-0"
+                width={30}
+                height={30}
+              />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <User className="mr-2 h-4 w-4" />
+                  <Link href="/profile">Profile</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <CreditCard className="mr-2 h-4 w-4" />
+                  <Link href="/subscriptions">Subscriptions</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Settings className="mr-2 h-4 w-4" />
+                  <Link href="/settings">Settings</Link>{" "}
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="cursor-pointer"
+                onClick={() => SignOut()}
+              >
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Log out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </aside>
+    </div>
+  );
+};
+
+export default Navbar2;
--- src/components/global/Sidebar.tsx
+++ src/components/global/Sidebar.tsx
@@ -1,5 +1,3 @@
-"use client";
-import Link from "next/link";
 import React, { useEffect } from "react";
 import { navigation } from "@/lib/utils";
 import { usePathname } from "next/navigation";
@@ -18,129 +16,26 @@ import {
 } from "../ui/dropdown-menu";
 import { CreditCard, LogOut, Settings, User } from "lucide-react";
 import { getRole, SignOut } from "@/app/actions/auth.actions";
+import Navbar2 from "./Navbar2";
+import { auth } from "../../../auth";
+import prisma from "@/lib/db";
 
 type Props = {};
 
-const Sidebar = (props: Props) => {
-  const pathName = usePathname();
-  const session = useSession();
+const Sidebar = async (props: Props) => {
+  const session = await auth();
 
-  // const user = session.data?.user;
-  // const acceptRole = async () => {
-  //   const role = await getRole();
-  //   return role;
-  //   console.log(role);
-  // };
+  const user = await prisma.user.findUnique({
+    where: { email: session?.user?.email || "" },
+  });
 
-  // const role = acceptRole();
+  const role = user?.role || "USER";
 
-  useEffect(() => {
-    if (!session.data?.user) {
-      window.location.href === "/sign-up";
-    }
-  }, []);
-  console.log(session.data?.user);
-  console.log(session.data?.user.role);
-  return (
-    <div className="p-4 flex items-center justify-between sticky top-0 backdrop-blur-md backdrop-brightness-75 border-b">
-      <aside className="flex items-center gap-2">
-        <Link href="/">
-          <span className="text-xl text-primary font-bold">Flow.</span>
-        </Link>
-      </aside>
+  if (!session?.user) {
+    window.location.href === "/sign-up";
+  }
 
-      <nav className="md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
-        <ul className="flex items-center justify-center md:gap-6 gap-3">
-          {navigation.map((link) => (
-            <Link
-              className={clsx(
-                "hover:border-b hover:text-primary hover:py-5 hover:border-primary",
-                {
-                  "text-primary py-5 border-b border-primary hover:border-orange-400 hover:text-orange-400":
-                    pathName === link.href,
-                }
-              )}
-              key={link.title}
-              href={link.href}
-            >
-              {link.title}
-            </Link>
-          ))}
-        </ul>
-        {session.data?.user.role === "ADMIN" && (
-          <Link
-            href="/add"
-            className={clsx(
-              "hover:border-b hover:text-primary hover:py-5 hover:border-primary",
-              {
-                "text-primary py-5 border-b border-primary hover:border-orange-400 hover:text-orange-400":
-                  pathName === "/add",
-              }
-            )}
-          >
-            Add
-          </Link>
-        )}
-      </nav>
-      <aside className="flex gap-2 items-center">
-        {session.data?.user && (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Image
-                src={session.data?.user?.image || ""}
-                alt="profile pic"
-                className="rounded-full cursor-pointer shrink-0"
-                width={30}
-                height={30}
-              />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  <User className="mr-2 h-4 w-4" />
-                  <Link href="/profile">Profile</Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <CreditCard className="mr-2 h-4 w-4" />
-                  <Link href="/subscriptions">Subscriptions</Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Settings className="mr-2 h-4 w-4" />
-                  <Link href="/settings">Settings</Link>{" "}
-                </DropdownMenuItem>
-                {session.data?.user.role === "ADMIN" && (
-                  <DropdownMenuItem>
-                    <Link
-                      href="/add"
-                      className={clsx(
-                        "hover:border-b hover:text-primary hover:py-5 hover:border-primary",
-                        {
-                          "text-primary py-5 border-b border-primary hover:border-orange-400 hover:text-orange-400":
-                            pathName === "/add",
-                        }
-                      )}
-                    >
-                      Add
-                    </Link>
-                  </DropdownMenuItem>
-                )}
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem
-                className="cursor-pointer"
-                onClick={() => SignOut()}
-              >
-                <LogOut className="mr-2 h-4 w-4" />
-                <span>Log out</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-      </aside>
-    </div>
-  );
+  return <Navbar2 role={role} />;
 };
 
 export default Sidebar;
