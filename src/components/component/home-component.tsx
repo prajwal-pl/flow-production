240ff18a969fd4723b6e9abf0ed2985dc69e7b99
Prajwal-PL <prajwalpl096@gmail.com>
2024-07-21 20:55:15 +0530

    fixed typos

--- src/components/component/home-component.tsx
+++ src/components/component/home-component.tsx
@@ -151,7 +151,7 @@ export function HomeComponent() {
               </div>
               <div className="flex flex-col gap-2 min-[400px]:flex-row">
                 <Link
-                  href="/dashbord"
+                  href="/dashboard"
                   className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                 >
                   Explore Dashboard
